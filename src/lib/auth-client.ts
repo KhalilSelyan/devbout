51a56d0f78f9487f3fb502c6a667d7208fba6828
Khalil Selyan <khalilselyan2@gmail.com>
2024-11-19 13:52:44 +0300

    use env for auth url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,5 @@
+import { env } from '$env/dynamic/public';
 import { createAuthClient } from 'better-auth/svelte';
 export const authClient = createAuthClient({
-	baseURL: 'http://localhost:5173'
+	baseURL: env.PUBLIC_BETTER_AUTH_URL
 });
