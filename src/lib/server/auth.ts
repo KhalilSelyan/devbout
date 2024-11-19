2a770794d483bb711fac9feff3ca1912538234d6
Khalil Selyan <khalilselyan2@gmail.com>
2024-11-19 14:15:21 +0300

    fix env uses

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { db } from './db';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { env } from '$env/dynamic/private';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import * as schema from './db/schema';
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -10,8 +10,8 @@ export const auth = betterAuth({
 	}),
 	socialProviders: {
 		google: {
-			clientId: env.GOOGLE_CLIENT_ID!,
-			clientSecret: env.GOOGLE_CLIENT_SECRET!
+			clientId: GOOGLE_CLIENT_ID!,
+			clientSecret: GOOGLE_CLIENT_SECRET!
 		}
 	}
 });
