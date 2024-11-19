26ac3b6de4ddb98540cdbdccc2231944b50e0b59
Khalil Selyan <khalilselyan2@gmail.com>
2024-11-19 19:29:31 +0300

    add shadcn/kitroute/viewtransitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { setupViewTransition } from 'sveltekit-view-transition';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
+
+	setupViewTransition();
 	let { children, data } = $props();
 </script>
 
@@ -10,3 +13,16 @@
 	{@render children()}
 	<Footer />
 </div>
+
+<style>
+	/* Disable default crossfade. */
+	:root {
+		view-transition-name: 'header';
+	}
+
+	/* Or, just modify the duration. */
+	:global(::view-transition-old(root)),
+	:global(::view-transition-new(root)) {
+		animation-duration: 0.5s;
+	}
+</style>
