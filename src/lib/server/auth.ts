33bfdf92418f80df16cf22dd2d9ab008c1dc79ae
Khalil Selyan <khalilselyan2@gmail.com>
2024-11-19 14:38:46 +0300

    use betterauthsecret and use adapter node

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,13 +1,14 @@
 import { betterAuth } from 'better-auth';
 import { db } from './db';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { BETTER_AUTH_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import * as schema from './db/schema';
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: 'pg',
 		schema
 	}),
+	secret: BETTER_AUTH_SECRET,
 	socialProviders: {
 		google: {
 			clientId: GOOGLE_CLIENT_ID!,
