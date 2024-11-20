9fb9a424700750e7d7e1e6e5177ad26ecfc4568e
Khalil Selyan <khalilselyan2@gmail.com>
2024-11-20 03:20:25 +0300

    update drizzle connection with schema

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
+import * as schema from './schema';
 import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
+
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
 const client = postgres(DATABASE_URL);
-export const db = drizzle(client);
+
+export const db = drizzle(client, { schema });
