bee9c0784ff0b3e332d2be48c926d6910d7458fa
Khalil Selyan <khalilselyan2@gmail.com>
2024-11-25 11:20:12 +0300

    potential prod build fix ?

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { kitRoutes } from 'vite-plugin-kit-routes';
 import { defineConfig } from 'vite';
 export default defineConfig({
-	plugins: [sveltekit(), kitRoutes()]
+	plugins: [sveltekit(), kitRoutes()],
+	build: {
+		rollupOptions: {
+			external: ['@reown/appkit-siwe']
+		}
+	}
 });
