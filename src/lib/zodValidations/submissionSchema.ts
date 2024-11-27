0da1c74a66eab8d886b5568763920044c10ed123
Khalil Selyan <khalilselyan2@gmail.com>
2024-11-27 22:30:54 +0300

    update submission schema

--- src/lib/zodValidations/submissionSchema.ts
+++ src/lib/zodValidations/submissionSchema.ts
@@ -5,10 +5,9 @@ export const submissionSchema = z.object({
 	description: z.string().min(1).max(1000),
 	teamId: z.string().min(1),
 	projectName: z.string().min(3, { message: 'Project name must be at least 3 characters long' }),
-	title: z.string().min(1).max(100),
 	githubUrl: z.string().url().optional(),
 	submissionUrl: z.string().url().optional(),
-	score: z.number().gt(0, { message: 'Should be Non-negative value' }).optional()
+	score: z.number().gte(0, { message: 'Should be Non-negative value' }).optional()
 });
 
 export type SubmissionSchema = typeof submissionSchema;
