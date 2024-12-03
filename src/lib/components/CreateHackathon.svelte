457ec682ca28c620050dbd95597be272ea181223
Khalil Selyan <khalilselyan2@gmail.com>
2024-12-03 23:27:14 +0300

    add loading state and loading spinner

--- src/lib/components/CreateHackathon.svelte
+++ src/lib/components/CreateHackathon.svelte
@@ -108,11 +108,10 @@
 					requestParameters: reqParams
 				});
 
-				// THIS RETURNED AN ERROR CYCLIC SOMETHING AFTER WAITING A BIT AND DIDNT GET TO CALL THE TRPC MUTATIONs
-				console.log(reshandle.requestId);
-				// ON CONFIRM PAYMENT DONE , CALL TRPC ENDPOINT FOR TRANSFERING MONEY FROM PLATFORM WALLET TO SMARTCONTRACT.
+				console.log({ requestId: reshandle.requestId });
 
-				await $createHackathonMutation.mutateAsync({
+				// ON CONFIRM PAYMENT DONE , CALL TRPC ENDPOINT FOR TRANSFERING MONEY FROM PLATFORM WALLET TO SMARTCONTRACT.
+				$createHackathonMutation.mutate({
 					hackathonId: _hackathonId,
 					isCrowdfunded: _isCrowdfunded,
 					basePrize
@@ -130,6 +129,7 @@
 	let isDialogOpen = $state(false);
 	let hackathonId = $state(nanoid());
 	let isSubmitting = $state(false);
+	let submitButtonText = $state('Create Hackathon');
 	// Create superForm with improved configuration
 	const sf = superForm(data, {
 		validators: zod(hackathonSchema),
@@ -137,6 +137,7 @@
 		resetForm: false,
 		onSubmit: async ({ cancel, jsonData, formData }) => {
 			isSubmitting = true;
+			submitButtonText = 'Submitting';
 			// Send all data, not just tainted fields
 			const allData = Object.fromEntries(Object.entries($form));
 			if (String(formData.get('status')) !== 'DRAFT') {
@@ -145,9 +146,10 @@
 					_isCrowdfunded: Boolean(formData.get('fundingType')),
 					basePrize: String(formData.get('basePrize'))
 				});
-				console.log({ hasGoneThrough });
+
 				if (!hasGoneThrough) {
 					isSubmitting = false;
+					submitButtonText = 'Create Hackathon';
 					cancel();
 				}
 			}
@@ -159,6 +161,7 @@
 			if (form.valid) {
 				console.log('Form submitted successfully', form.data);
 				trpc.hackathon.getHackathons.utils.invalidate();
+				submitButtonText = 'Create Hackathon';
 				isDialogOpen = false;
 			}
 
@@ -264,7 +267,9 @@
 		Create Hackathon
 	</Dialog.Trigger>
 	<Dialog.Overlay
-		onclick={() => {
+		onclick={(e) => {
+			e.preventDefault();
+			e.stopPropagation();
 			if (!isSubmitting) isDialogOpen = false;
 		}}
 	/>
@@ -472,8 +477,33 @@
 					variant="destructive">Cancel</Button
 				>
 
-				<Button type="submit" disabled={!isValidCriteria() || isSubmitting}>Create Hackathon</Button
+				<Button
+					class="flex items-center gap-2"
+					type="submit"
+					disabled={!isValidCriteria() || isSubmitting}
 				>
+					{#if submitButtonText === 'Submitting'}
+						<span class="motion-preset-spin motion-duration-2000 motion-loop-infinite">
+							<svg
+								aria-hidden="true"
+								class="h-8 w-8 fill-emerald-600 text-gray-200 dark:text-gray-600"
+								viewBox="0 0 100 101"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+									fill="currentColor"
+								/>
+								<path
+									d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+									fill="currentFill"
+								/>
+							</svg>
+						</span>
+					{/if}
+					<span>{submitButtonText}</span>
+				</Button>
 			</div>
 		</form>
 	</Dialog.Content>
